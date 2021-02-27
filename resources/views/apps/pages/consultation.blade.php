2051b2ddbd8e42a1d9d1d1dd91c89acd6f4f96e8
owenwattimena <wentoxwtt@gmail.com>
2021-02-27 14:34:15 +0900

    modifikasi fitur search

--- resources/views/apps/pages/consultation.blade.php
+++ resources/views/apps/pages/consultation.blade.php
@@ -2,8 +2,9 @@
 
 @section('style')
     <!-- Data Table -->
-    <link href="cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-    <link href="{{asset('assets/pakar/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    {{-- <link href="{{asset('assets/pakar/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet"> --}}
 @endsection 
 
 @section('content')
@@ -105,13 +106,15 @@
 
 @section('script')
     <!-- Datatables -->
-    <script src="{{asset('assets/pakar/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('assets/pakar/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
-    {{-- <script src="{{asset('assets/pakar/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')}}"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
 
     <script>
         $(document).ready( function () {
-            $('#dataTable').DataTable();
+            $('#dataTable').DataTable({
+                "paging" : false
+            });
         } );
         $('.form-check-input').click(function(e){
             if (this.getAttribute('checked')) { // check the presence of checked attr
